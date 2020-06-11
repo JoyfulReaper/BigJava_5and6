d1777eb1a9ed1ba78144c6b8ab40648845e31e9c
Kyle Givler <xvegan88x@gmail.com>
2020-06-11 19:04:33 -0400

    Prints a name in a box :p

---
+++ Chapter1/NamePrinter.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class NamePrinter {
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter your name: ");
+        String name = in.nextLine();
+
+        System.out.println("Your name is " + name);
+        System.out.println("");
+
+        printLines(name.length());
+        System.out.println("\n|" + name + "|");
+        printLines(name.length());
+
+        in.close();
+    }
+
+    public static void printLines(int number) 
+    {
+        System.out.print(" ");
+        for(int i = 0; i < number; i++)
+        {
+            System.out.print("-");
+        }
+    }
+}
