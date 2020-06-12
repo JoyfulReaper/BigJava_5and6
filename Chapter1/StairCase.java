3d582c00964064576f2a94ca66ecf254ec051a93
Kyle Givler <xvegan88x@gmail.com>
2020-06-11 20:15:08 -0400

    Ask how many stairs

--- Chapter1/StairCase.java
+++ Chapter1/StairCase.java
@@ -1,16 +1,23 @@
+import java.util.Scanner;
+
 public class StairCase {
     public static void main(String[] args)
     {
-        printBoxes(4);
+        Scanner in = new Scanner(System.in);
+        System.out.print("How many stairs? ");
+
+        printStairs(in.nextInt());
     }
 
-    public static void printBoxes(int boxes)
+    public static void printStairs(int boxes)
     {
+        int boxWidth = 5;
         for(int i = 0; i < boxes; i++)
         {
-            int spacesBefore = (4 * (boxes - i) - 4);
+            int spacesBefore = ((boxWidth - 1) * (boxes - i) - (boxWidth - 1));
             for(int j = 0; j <= i; j++)
             {
+                // Print the top
                 if(j == 0)
                     System.out.print(" ".repeat(spacesBefore));
                 System.out.print("+---");
@@ -19,12 +26,14 @@ public class StairCase {
 
             for(int j = 0; j <=i; j++)
             {
+                // Print the sides
                 if(j == 0)
                     System.out.print(" ".repeat(spacesBefore));
                 System.out.print("|   ");
             }
             System.out.print("|\n");
         }
+        // Print the bottom
         for(int i = 0; i < boxes; i++)
             System.out.print("+---");
             System.out.println("+");
