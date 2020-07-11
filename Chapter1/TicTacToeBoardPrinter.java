8a649e9414e48e6a7cf11217128769853cdd0a29
Kyle Givler <xvegan88x@gmail.com>
2020-07-11 15:07:39 -0400

    TacTacToe board printer! Why not?

--- Chapter1/TicTacToeBoardPrinter.java
+++ Chapter1/TicTacToeBoardPrinter.java
@@ -1,3 +1,24 @@
 public class TicTacToeBoardPrinter {
-    
+    public static void main(String[] args)
+    {
+        for(int i = 0; i < 3; i++)
+        {
+            printBoxTops(3);
+            printBoxSides(3);
+        }
+        printBoxTops(3);
+    }
+
+    private static void printBoxTops(int n)
+    {
+        for(int i = 0; i < n; i++)
+            System.out.print("+---");
+        System.out.println("+");
+    }
+    private static void printBoxSides(int n)
+    {
+        for(int i = 0; i < n; i++)
+            System.out.print("|   ");
+        System.out.println("|");
+    }
 }
